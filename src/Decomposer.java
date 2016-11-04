d461dd383086dac8e42b76a64f20aa291521692f
employee <vadymz@softwareplanet.uk.com>
2016-11-04 12:59:50 +0200

    complete

--- src/Decomposer.java
+++ src/Decomposer.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
+
 /**
  * Created by employee on 11/4/16.
  */
 class Decomposer {
 
     public static void main(String[] args) {
-      add(1);
+        System.out.println(add(20));
     }
-    static int add(int number) {
-        if(number==1){
-            System.out.println(number);
-            return number;
+
+    static ArrayList<Integer> add(int number) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int check = 2;
+        if (number < 0)
+            throw new RuntimeException("inputted a number = " + number);
+        if(number==0)
+            throw new RuntimeException("inputted a number = " + number);
+        if (number ==1) {
+            list.add(number);
+            return list;
         }
-        return number;
+        int i=check;
+        for (;i <= number;) {
+            if (number % i == 0) {
+                list.add(i);
+                number = number / i;
+            }
+            else i+=1;
+        }
+        return list;
     }
+
 }
